42bc235268016f74c8385ce1780214de5bab0252
Alexander Nalivayko <waardk@gmail.com>
2021-12-01 15:31:00 +0300

    wanago_1

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import PostsService from './posts.service';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto'
+
+@Controller('posts')
+export default class PostsController {
+    constructor(
+        private readonly postsService: PostsService
+    ) {}
+
+    @Get()
+    getAllPosts() {
+    return this.postsService.getAllPosts();
+    }
+
+    @Get(':id')
+    getPostById(@Param('id') id: string) {
+        return this.postsService.getPostById(Number(id));
+    }
+
+    @Post()
+    async createPost(@Body() post: CreatePostDto) {
+        return this.postsService.createPost(post);
+    }
+    
+    @Put(':id')
+    async replacePost(@Param('id') id: string, @Body() put: UpdatePostDto) {
+        return this.postsService.replacePost(Number(id), put);
+    }
+
+    @Delete(':id')
+    async deletePost(@Param('id') id: string) {
+        return this.postsService.deletePost(Number(id));
+    }
+}
+
+
+
